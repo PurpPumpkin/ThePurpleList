30266da40d457c01a50c11c6ec4bde99d6cb6e6a
PurpPumpkin <tobeymichael555@icloud.com>
2026-08-13 20:20:55 -0700

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,12 +1,18 @@
-import List from '/ThePurpleList/js/pages/List.js';
-import Leaderboard from '/ThePurpleList/js/pages/Leaderboard.js';
-import Roulette from '/ThePurpleList/js/pages/Roulette.js';
+import { fetchList } from './content.js';
+import { fetchScore } from './score.js';
+import route from './routes.js';
+import store from './store.js';
 
-export default VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(),
-    routes: [
-        { path: '/', component: List },
-        { path: '/leaderboard', component: Leaderboard },
-        { path: '/roulette', component: Roulette },
-    ],
+const app = Vue.createApp({
+    data() {
+        return {
+            store,
+        };
+    },
+    mounted() {
+        document.title = 'The Purple List';
+    },
 });
+
+app.use(route);
+app.mount('#app');
