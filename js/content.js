42cc567c79c1f33df7d95327feb471b8466f457c
PurpPumpkin <tobeymichael555@icloud.com>
2026-08-13 20:07:45 -0700

    Updated content.js 2

--- js/content.js
+++ js/content.js
@@ -1,23 +1,4 @@
-export async function fetchList() {
-    const listJson = await fetch('/ThePurpleList/data/_list.json');
-    const list = await listJson.json();
-    return list;
-}
-
-export async function fetchMainList() {
-    const listJson = await fetch('/ThePurpleList/data/_list.json');
-    const list = await listJson.json();
-
-    return Promise.all(
-        list.map(async (path, index) => {
-            const rank = index + 1;
-            try {
-                const levelJson = await fetch(`/ThePurpleList/data/${path}.json`);
-                const level = await levelJson.json();
-                return [null, { rank, path, ...level }];
-            } catch (err) {
-                return [path, null];
-            }
-        }),
-    );
-}
+export const config = {
+    // This defines the core root folder location for your data assets
+    root: '/ThePurpleList',
+};
