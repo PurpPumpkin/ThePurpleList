0c1e65c570ea402fba057061a230905a32e50ec9
PurpPumpkin <tobeymichael555@icloud.com>
2026-08-13 20:39:12 -0700

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,7 +1,49 @@
-import { fetchList } from './content.js';
+/**
+ * Numbers of decimal digits to round to
+ */
+const scale = 3;
 
-export async function fetchScore() {
-    const scoreJson = await fetch('/ThePurpleList/data/_players.json');
-    const score = await scoreJson.json();
-    return score;
+/**
+ * Calculate the score awarded when having a certain percentage on a list level
+ * @param {Number} rank Position on the list
+ * @param {Number} percent Percentage of completion
+ * @param {Number} minPercent Minimum percentage required
+ * @returns {Number}
+ */
+export function score(rank, percent, minPercent) {
+    if (rank > 150) {
+        return 0;
+    }
+    if (rank > 75 && percent < 100) {
+        return 0;
+    }
+
+    // New formula
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+
+    score = Math.max(0, score);
+
+    if (percent != 100) {
+        return round(score - score / 3);
+    }
+
+    return Math.max(round(score), 0);
+}
+
+export function round(num) {
+    if (!('' + num).includes('e')) {
+        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
+    } else {
+        var arr = ('' + num).split('e');
+        var sig = '';
+        if (+arr[1] + scale > 0) {
+            sig = '+';
+        }
+        return +(
+            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
+            'e-' +
+            scale
+        );
+    }
 }
