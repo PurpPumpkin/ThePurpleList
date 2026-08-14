169d7ed261ab0dee7a2edca3fe250af59fee37cf
PurpPumpkin <tobeymichael555@icloud.com>
2026-08-13 19:56:54 -0700

    Updated routes.js 2

--- js/routes.js
+++ js/routes.js
@@ -1,3 +1,7 @@
+import List from './pages/List.js';
+import Leaderboard from './pages/Leaderboard.js';
+import Roulette from './pages/Roulette.js';
+
 export default VueRouter.createRouter({
     history: VueRouter.createWebHistory('/ThePurpleList/'),
     routes: [
